94ac95f71dba031657fe6ec7491b864c1c2f2bad
Joshua Smith <smithj92@cardiff.ac.uk>
2023-12-28 18:51:54 +0000

    Created main.tf and added tf lock file

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Learn: "Terraform init" to initialise the back-end
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
