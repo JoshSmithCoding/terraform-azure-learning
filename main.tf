23c7722192d5e2207c0fc949608706f810d264c9
Joshua Smith <smithj92@cardiff.ac.uk>
2023-12-30 14:47:52 +0000

    Added network security group & rule

--- main.tf
+++ main.tf
@@ -43,3 +43,28 @@ resource "azurerm_subnet" "mtc-subnet" {
   # Plural name for variable, probably expects list
   address_prefixes = ["10.123.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                   = "mtc-dev-rule"
+  priority               = 100
+  direction              = "Inbound"
+  access                 = "Allow"
+  protocol               = "*"
+  source_port_range      = "*"
+  destination_port_range = "*"
+  # The /32 represents the subnet mask, indicating that there is only a single IP address in the network, and it is not part of a subnet.
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
